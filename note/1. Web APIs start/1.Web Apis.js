221e1bedb6e7ee316cef06dc275d3f8fd9923869
florence1920 <jetaime12121@gmail.com>
2021-07-02 00:24:40 +0900

    0702

---
+++ note/1. Web APIs start/1.Web Apis.js
@@ -0,0 +1,10 @@
+// Web APIs 란?
+
+// Application Programming Interfaces
+// 정확한 로직을 모르더라도 간단하게 프로그램을 만들게 도와 주는 것!
+
+//ex) DOM, Network, Graphics, Audio/Video, Device, File, Storage APIs
+
+//HTTP(Hypertext Transfer Protocal)
+//클라이언트와 서버가 어떻게 동신하는지에 대한 규약을 정해 놓은 것
+//HTTPS(Secure) 보안 처리가 된 것 
---
+++ note/1. Web APIs start/2.Brower 구조 분석.js
@@ -0,0 +1,8 @@
+//Browser 구조 분석
+
+//window > dom,bom,js
+
+//browser 에서 window는 최고의 object 임!
+//window.  하지 않아도 window인식됨
+
+//주로 사용되는 것은 사이즈, 스크롤, 로드 확인 할 때 많이 쓰임 
